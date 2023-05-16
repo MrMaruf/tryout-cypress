62a01fed999ae409b68567047d38241bec04dc6c
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-16 08:55:52 +0200

    Added specific assert & comments for older versions

--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -4,15 +4,25 @@ import App from "./App";
 describe("<App />", () => {
   it("renders", () => {
     // see: https://on.cypress.io/mounting-react
+
     // Define the url to spy on and intercept on call
+    // On older versions on cypress use cy.route()
+    // cy.route("POST", "https://dummyjson.com/products/add")
+    // .as("testPost");
     cy.intercept({
       method: "POST",
       url: "https://dummyjson.com/products/add",
-    }).as("testPost");
+    })
+    .as("testPost");
 
 
     cy.mount(<App />);
     cy.get("#postRequestButton").click();
+    // on older versions of cypress use cy.get('@testPost')
     cy.wait(['@testPost'])
+    .then((interception)=>{
+      assert.equal(interception.response.statusCode, 200) 
+      assert.isNotNull(interception.response.body)
+    })
   });
 });
